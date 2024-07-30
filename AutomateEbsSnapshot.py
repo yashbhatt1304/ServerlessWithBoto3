a9e58f63ccbfae7db5d9ca6b989deb743bfc0647
Yash_Bhatt <yashbhatt1304@gmail.com>
2024-07-31 01:35:50 +0530

    Updated the functions

--- AutomateEbsSnapshot.py
+++ AutomateEbsSnapshot.py
@@ -1,22 +1,19 @@
-import json
 import boto3
 from datetime import datetime, timezone, timedelta
 
 ec2=boto3.client('ec2')
 
-currentDate = datetime.now(timezone.utc)
+currentDate = datetime.strptime(datetime.today().strftime('%d %b %Y %H:%M:%S'), '%d %b %Y %H:%M:%S')
 print("Today's date is", currentDate)
-thresholdDate = (currentDate - (timedelta(days=7)))
+thresholdDate = (currentDate - (timedelta(days=1)))
 print("Threshold date is", thresholdDate)
 
 def lambda_handler(event, context):
     volIds = event["VolumeIds"]
     for vol in volIds:
+        # createSnapshots(vol)
         descSnapshots(vol)
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
-    }
+
 
 def descSnapshots(volume):
     response = ec2.describe_snapshots(
@@ -24,8 +21,22 @@ def descSnapshots(volume):
             {'Name': 'volume-id', 'Values': [volume]}
             ]
     )
-    date = response['ResponseMetadata']['HTTPHeaders']['date']
-    format = '%a, %d %b %Y %H:%M:%S'
-    formatedDate = datetime.strptime(date[:-4], format) 
-    print(f"VolumeId: {volume} created on {formatedDate}")
+    # print(response)
+    for snapshot in response['Snapshots']:
+        date = snapshot["StartTime"].replace(tzinfo=None)
+        # format = '%a, %d %b %Y %H:%M:%S'
+        # formatedDate = datetime.strptime(date[:-4], format) 
+        print(f"SnapshotId: {snapshot['SnapshotId']} created on {date}")
+        if (date<thresholdDate):
+            deleteSnapshot(snapshot['SnapshotId'])
+            print(f"Snapshot {snapshot['SnapshotId']} has passed the retention period.")
 
+def createSnapshots(volume):
+    ec2.create_snapshot(
+        VolumeId=volume,
+        Description=f"Snapshot of volume {volume} by Yash"
+            )
+
+def deleteSnapshot(snapshot):
+    ec2.delete_snapshot(SnapshotId=snapshot)
+    print(f"Deleted {snapshot}")
