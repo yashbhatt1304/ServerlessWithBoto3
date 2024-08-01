91793a3036334b34bfd49eee99c5c1ba1a63d7a7
Yash_Bhatt <yashbhatt1304@gmail.com>
2024-08-02 01:44:54 +0530

    Updating AutoTagEC2

--- AutoTagEC2.py
+++ AutoTagEC2.py
@@ -1,18 +1,37 @@
 import json
 import boto3
+from datetime import datetime, timezone, timedelta
 
 ec2 = boto3.client('ec2')
 
 def lambda_handler(event, context):
     # TODO implement
-    getInstance()
+    InstnaceList = getInstances()
     return {
         'statusCode': 200,
         'body': json.dumps('Hello from Lambda!')
     }
 
-def getInstances(throsholdTime):
-    print(f"Fetching List of Instances created after {throsholdTime}")
+def getInstances():
+    list=[]
+    instances = ec2.describe_instances()
+    for reservation in instances['Reservations']:
+        for instance in reservation['Instances']:
+            if instance['State']['Name'] == 'running':
+                launchTime = instance['LaunchTime']
+                thresholdTime = datetime.now(timezone.utc) - timedelta(minutes=5)
+                if (thresholdTime < launchTime):
+                    list.append(instance['InstanceId'])
+                    print(f"Instance {instance['InstanceId']} created after {thresholdTime}")
+                    attachTags(instance['InstanceId'], launchTime)
+    return list
     
-def attachTags(instanceId):
+
+def attachTags(instanceId, time):
+    ec2.create_tags(
+        Resources=[instanceId],
+        Tags=[{'Key': 'Launch_Time', 'Value': str(time)},
+        {'Key': 'CreatedBy', 'Value': "Auto-Tag-Lambda-By-Yash"}]
+        )
     print(f"Tags attached to {instanceId}")
+    
